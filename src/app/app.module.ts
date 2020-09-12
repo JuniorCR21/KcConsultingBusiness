0837373d8d40ed53822d4be8d8b85612c65ddc0b
JuniorCr21 <carrion_21_21utlook.es>
2020-09-11 19:30:27 -0500

    Paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { PrincipalComponent } from './paginas/principal/principal.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    PrincipalComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
