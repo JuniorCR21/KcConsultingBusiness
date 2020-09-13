b1097d5fd03f134a9f350c4e55a49201a5060e8b
JuniorCr21 <carrion_21_21utlook.es>
2020-09-13 03:32:45 -0500

    Avanzando principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { PrincipalComponent } from './paginas/principal/principal.component';
 import { ContactoComponent } from './paginas/contacto/contacto.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/Inicio', pathMatch: 'full'},
+  {path:'Inicio', component: PrincipalComponent},
+  {path:'Contacto', component: ContactoComponent}
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +27,9 @@ import { ContactoComponent } from './paginas/contacto/contacto.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
