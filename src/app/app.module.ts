3f2b989d33f391fadb17ac9f688fa36a29f086aa
JuniorCr21 <carrion_21_21utlook.es>
2020-09-16 19:39:05 -0500

    Casi terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
